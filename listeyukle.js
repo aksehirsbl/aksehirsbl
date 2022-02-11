3c0ae3418942b6cbbb15976d3c96863b41fe007b
aksehirsbl <99477086+aksehirsbl@users.noreply.github.com>
2022-02-11 16:26:55 +0300

    Update listeyukle.js

--- listeyukle.js
+++ listeyukle.js
@@ -4,7 +4,7 @@ function selectNext(selid,c){
 	select.dispatchEvent(new Event('change'));
 }
 
-var xmlFile="xml/veriler_20211217.xml";
+var xmlFile="xml/veriler_20220211.xml";
 
 var httpObj = new XMLHttpRequest();
 httpObj.open("GET",xmlFile,false);
